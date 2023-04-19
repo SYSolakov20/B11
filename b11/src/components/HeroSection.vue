715da73bc171fec30e4d973d4199855c298ca741
VTIvanov20 <VTIvanov20@codingburgas.bg>
2023-04-19 22:26:13 +0300

    Add splash heading & various fonts

--- b11/src/components/HeroSection.vue
+++ b11/src/components/HeroSection.vue
@@ -1,5 +1,8 @@
 <template>
-    <section></section>
+    <section>
+        <nav id="nav-parent"></nav>
+        <h1 id="heading">RECYCLE WITH US</h1>
+    </section>
 </template>
 
 <script lang="ts" setup>
@@ -7,9 +10,41 @@
 </script>
 
 <style lang="scss" scoped>
+    @font-face {
+        font-family: 'Caslon Regular Display';
+        src: url(../assets/fonts/F37Caslon-RegularDisplay.otf);
+    }
+
+    @font-face {
+        font-family: 'Caslon Semi Bold Hairline';
+        src: url(../assets/fonts/F37Caslon-SemiBoldHairline.otf);
+    }
+
+    @font-face {
+        font-family: 'Lato Regular';
+        src: url(../assets/fonts/F37Lato-Regular.ttf);
+    }
+
+    #nav-parent {
+        height: 10vh;
+        width: 100vw;
+        background-color: red;
+    }
+
+    #heading {
+        height: auto;
+        width: 100vw;
+        font-family: 'Caslon Semi Bold Hairline';
+        font-size: 5rem;
+        font-weight: 400;
+        font-style: normal;
+        line-height: 115px;
+        letter-spacing: 0.115em;
+    }
+
     section {
         height: 100vh;
         width: 100vw;
-        background-color: red;
+        background-color: white;
     }
 </style>
