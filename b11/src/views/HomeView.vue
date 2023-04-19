6f4b7e638f774d36d03be9d852b2875a46278e8a
Soner Solakov <sysolakov20@codingburgas.bg>
2023-04-19 20:32:21 +0300

    Edit Vue router

--- b11/src/views/HomeView.vue
+++ b11/src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <HeroSection />
 </template>
+
+<script lang="ts" setup>
+  import HeroSection from '@/components/HeroSection.vue';
+</script>
