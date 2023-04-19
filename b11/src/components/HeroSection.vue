c486a26b06c6d75806a51fd4c4008e1f500cd6b4
VTIvanov20 <VTIvanov20@codingburgas.bg>
2023-04-19 22:48:26 +0300

    Expand on main content

--- b11/src/components/HeroSection.vue
+++ b11/src/components/HeroSection.vue
@@ -1,7 +1,15 @@
 <template>
     <section>
-        <nav id="nav-parent"></nav>
         <h1 id="heading">RECYCLE WITH US</h1>
+        <div id="module-parent">
+            <div id="preview-left">previous</div>
+            <!-- <div id="mtr">
+                <div class="mtr-back"></div>
+                <div class="mtr-spotlight"></div>
+                <div class="mtr-next"></div>
+            </div> -->
+            <div id="preview-right">next</div>
+        </div>
     </section>
 </template>
 
@@ -19,21 +27,24 @@
         font-family: 'Caslon Semi Bold Hairline';
         src: url(../assets/fonts/F37Caslon-SemiBoldHairline.otf);
     }
-
+    
     @font-face {
         font-family: 'Lato Regular';
         src: url(../assets/fonts/F37Lato-Regular.ttf);
     }
 
-    #nav-parent {
-        height: 10vh;
+    section {
+        height: 100vh;
         width: 100vw;
-        background-color: red;
+        background-color: white;
     }
 
     #heading {
         height: auto;
         width: 100vw;
+        display: flex;
+        align-items: center;
+        justify-content: center;
         font-family: 'Caslon Semi Bold Hairline';
         font-size: 5rem;
         font-weight: 400;
@@ -42,9 +53,28 @@
         letter-spacing: 0.115em;
     }
 
-    section {
-        height: 100vh;
+    #module-parent {
         width: 100vw;
-        background-color: white;
+        height: 80vh;
+        display: flex;
+        background-color: blue;
     }
+
+    #preview-left, #preview-right {
+        height: inherit;
+        flex-direction: column;
+        text-align: center;
+        background-color: red;
+    }
+
+    #preview-left {
+        flex-direction: column;
+        writing-mode: sideways-lr;
+    }
+
+    #preview-right {
+        flex-direction: column;
+        writing-mode: sideways-rl;
+    }
+
 </style>
