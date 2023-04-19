e12e74962d7b44927005c6eaaa4dae16f1fca9ba
Soner Solakov <sysolakov20@codingburgas.bg>
2023-04-19 22:49:30 +0300

    Add nav

---
+++ b11/src/components/NavBar.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="nav-wrapper">
+        <div class="logo">B11</div>
+        <div class="text-wrapper">BUY / SELL / AQUIRE</div>
+        <div class="menu-button">Menu</div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    @font-face {
+        font-family: 'Caslon Regular Display';
+        src: url(../assets/fonts/F37Caslon-RegularDisplay.otf);
+    }
+    @font-face {
+        font-family: 'Caslon Semi Bold Hairline';
+        src: url(../assets/fonts/F37Caslon-SemiBoldHairline.otf);
+    }
+    @font-face {
+        font-family: 'Lato Regular';
+        src: url(../assets/fonts/Lato-Regular.ttf);
+    }
+    .nav-wrapper{
+        width: 100vw;
+        height: 10vh;
+        border-style: solid;
+        border-color: red;
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+    }
+    .logo{
+        font-family: "Caslon Semi Bold Hairline";
+        font-size: 3.75rem;
+    }
+    .text-wrapper{
+        font-family: "Lato Regular";
+        font-size: 1.5rem;
+    }
+    .menu-button{
+        font-family: "Lato Regular";
+        font-size: 1.4rem;
+        border: 2px dashed #919191;
+        width: 5rem;
+        height: 5rem;
+        border-radius: 50%;
+        text-align: center;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
+
+
+<script lang="ts" setup>
+
+</script>
--- b11/src/views/HomeView.vue
+++ b11/src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <template>
+    <NavBar />
     <HeroSection />
 </template>
 
 <script lang="ts" setup>
   import HeroSection from '@/components/HeroSection.vue';
+  import NavBar from '@/components/NavBar.vue';
 </script>
